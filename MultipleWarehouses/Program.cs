17265bf2b94e5260aefe2cd63cc33967da11c618
Doh09 <alexandreasen09@gmail.com>
2018-02-27 10:45:08 +0100

    Part of multiple warehouse initialization.

---
+++ MultipleWarehouses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultipleWarehouses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Task.Factory.StartNew(() => new Warehouse.Warehouse(1, "Danmark", "Esbjerg"));
+            Task.Factory.StartNew(() => new Warehouse.Warehouse(2, "Tyskland", "München"));
+            Task.Factory.StartNew(() => new Warehouse.Warehouse(3, "Norge", "Oslo"));
+            //Constructoren får parametre ind.
+            //Lokal retailer
+            //Hvis ikke lokal retailer, så søger den andre warehouses.
+            Console.ReadLine();
+        }
+    }
+}
