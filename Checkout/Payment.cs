e449455bf569dff919b504e700642645201f11fb
MartinDataStuff <martin1-g@hotmail.com>
2018-02-28 12:37:51 +0100

    Added functionality to the checkout system

--- Checkout/Payment.cs
+++ Checkout/Payment.cs
@@ -1,18 +1,38 @@
-﻿using System;
+﻿using Basket;
+using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Text;
+using ShoppingSystem_Entities;
 
 namespace Checkout
 {
     class Payment
     {
-        public double Cost()
+        private BasketManager _basketManager;
+        private Shipment _shipment;
+        public List<Product> ListOfProducts(string userID)
         {
-            return 0;
+            _basketManager.CreateObjectPool(10);
+
+            _basketManager.GetObjectPool(userID);
+
+            //Doesn't show list, list is private, talk with Dennis later
+            var list = BasketManager.activelist[userID];
+            return null;
+
         }
-        public double CostWithShipment()
+        public int TotalCost(List<Product> list)
         {
-            return 0;
+            int cost = 0;
+            foreach (var product in list)
+                cost += product.Price;
+            return cost;
+        }
+        public int? CostWithShipment(List<Product> list, Location to)
+        {
+            _shipment = new Shipment();
+            return _shipment.Cost(list, to) + TotalCost(list);
         }
     }
 }
