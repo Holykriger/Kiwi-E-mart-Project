2810b76e7b64e98fb014ee10120d5189cab4e10d
Simon Bay Nymand <simon.bay@live.dk>
2018-03-09 16:29:18 +0100

    Fixed lidt på WarehouseController
    gjore den bare letter at finde

--- Checkout/WareHouseConegtion.cs
+++ Checkout/WareHouseConegtion.cs
@@ -8,10 +8,15 @@ namespace Checkout
     public class WareHouseConegtion
     {
         // the incoming prouct is use i wareHouse Controller as a description of what the customor wants,
-        public List<Product> GetProdukts(Product product)
+        public List<List<Product>> GetProdukts(List<Product> Wantedproducts)
         {
+            List<List<Product>> ProduktsList = new List<List<Product>>();
             List<Product> ProduktList = new List<Product>();
-            
+            WareHouseController WHC = GetWarhousecontroller();
+            foreach (var wantedproduct in Wantedproducts)
+            {
+                WHC.
+            }
             //Get WareHouse Controller and use FindProduct(product)
             return ProduktList;
         }
